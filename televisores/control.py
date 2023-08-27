87b05ef54bab57a385b3f466060c4f812e0515ff
sebasz-a <jpabuena@unal.edu.co>
2023-08-26 23:30:04 -0500

    Prueba 3

--- televisores/control.py
+++ televisores/control.py
@@ -22,10 +22,12 @@ class Control:
             self._tv.volumen -= 1
 
     def setCanal(self, canal):
-        self._tv.canal = canal
+        if (self._tv.estado == True) and (canal >= 1 and canal <= 120):
+            self._tv.canal = canal
 
     def setVolumen(self, volumen):
-        self._tv.volumen = volumen
+        if (self._tv.estado == True) and (volumen >= 0 and volumen <= 7):
+            self._tv.volumen = volumen
 
     def enlazar(self, tv):
         self._tv = tv
